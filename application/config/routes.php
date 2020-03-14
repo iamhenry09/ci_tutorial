36a3cf445cee60f5add2ba4efea12c7fea643ca6
iamhenry09 <henryportus9@gmail.com>
2020-03-14 22:35:46 +0800

    news section

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'pages/view';
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
--- application/controllers/News.php
+++ application/controllers/News.php
@@ -13,13 +13,23 @@
             $data['news'] = $this->news_model->get_news();  
 
             $data['title'] = 'News Archive';
-            $this->load->view('template/header',$data);
+            $this->load->view('templates/header',$data);
             $this->load->view('news/index', $data);
-            $this->load->view('template/footer');
+            $this->load->view('templates/footer');
         }
 
         public function view($slug = NULL){
             $data['news_item'] = $this->news_model->get_news($slug);
+
+            if(empty($data['news_item'])){
+                show_404();
+            }
+
+            $data['title'] = $data['news_item']['title'];
+
+            $this->load->view('templates/header',$data);
+            $this->load->view('news/view',$data);
+            $this->load->view('templates/footer');
         }
     }
 
